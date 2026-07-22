2022bdc36ab145e4d09818c7c73d39f0eaed23a1
Prashant Malagi <prashantmalagi60@gmail.com>
2026-07-22 10:20:25 +0530

    Add MyString class with example of string manipulation

---
+++ MyString.java
@@ -0,0 +1,7 @@
+public class MyString {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        System.out.println(" String: " + str);
+        System.out.println(" String length: " + str.length());
+    }
+}
