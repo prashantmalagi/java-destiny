b32b3ac989197f9ae431a0a57ca8e1717693b90e
Prashant Malagi <prashantmalagi60@gmail.com>
2026-07-22 10:54:23 +0530

    Refactor MyString class and add example of string manipulation

--- String.java
+++ String.java
@@ -1,6 +1,7 @@
 public class MyString {
-    public static void main(java.lang.String[] args) {
-        java.lang.String str = "Hello, World!";
+    public static void main(String[] args) {
+        MyString a = new MyString();
+        String str = "Hello, World!";
         System.out.println(" String: " + str);
         System.out.println(" String length: " + str.length());
         System.out.println("Lowercase: " + str.toLowerCase());
@@ -9,9 +10,8 @@ public class MyString {
         System.out.println("INDEX OF ' ' : " + str.indexOf(' '));
         System.out.println("Starts with 'Hello': " + str.startsWith("Hello"));
         System.out.println("Ends with 'World!': " + str.endsWith("World!"));
-        System.out.println("Equals 'Hello, World!': " + str.equals("Hello,World!"));
+        System.out.println("Equals 'Hello, World!': " + str.equals("Hello, World!"));
         System.out.println("Substring from index 7: " + str.substring(7));
         System.out.println("Substring from index 7 to 12: " + str.substring(7, 12));
-
     }
 }
